f8210e0f8dcb7fb7dc1b4186a65c929f4c03c774
Matas Kavaliauskas <matas.kavaliauskas86@gmail.com>
2026-02-25 21:01:59 +0100

    feat: initialize Spring Boot project structure

---
+++ src/main/java/lt/example/insurance/SocialInsuranceContributionsApiApplication.java
@@ -0,0 +1,13 @@
+package lt.example.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialInsuranceContributionsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialInsuranceContributionsApiApplication.class, args);
+    }
+}
+
